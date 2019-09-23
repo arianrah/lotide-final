c12963c5d4c5f8b9fbf809979af6b2b6be049a91
Arian Rahimi <arianrahimi@icloud.com>
2019-09-23 23:32:00 +0000

    Updated up to middleTest

--- tail.js
+++ tail.js
@@ -1,6 +1,5 @@
 const tail = function(words) {
   const array = words.slice(1)
-  //const tail = Array.from(array);
   return array
 }
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+
+  it('returns [lighthouse, Labs] from [Yo Yo, Lighthouse, Labs]', () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  })
+
+})
 
 
-
+/*
 const words = ['Yo Yo', 'Lighthouse', 'Labs']
 tail(words) // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3) // original array should still have 3 elements!
+*/
